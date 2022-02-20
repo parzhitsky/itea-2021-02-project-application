151b5c9d2fe4a2e898cf7dcc7011c346e24b30cd
Dima Parzhitsky <parzhitsky@gmail.com>
2022-02-20 17:53:47 +0200

    Remove nonsensical unit test

--- src/app.unit.ts
+++
@@ -1,9 +0,0 @@
-import http = require("http");
-import app from "./app";
-
-describe("app", () => {
-	it("should be an Express application", () => {
-		expect(app).toHaveProperty("listen", expect.any(Function));
-		expect(app.listen(8081).close()).toBeInstanceOf(http.Server);
-	});
-});
