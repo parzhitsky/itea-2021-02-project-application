adf2c3484f4b0c39be8621843d1242cf9ba13d1d
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2022-03-26 19:01:52 +0200

    Allow connecting to a local database

--- src/db/client.ts
+++ src/db/client.ts
@@ -4,12 +4,21 @@ import logger from "../log/logger";
 
 export * from "sequelize";
 
+/** @private */
+type Env = NodeJS.ProcessEnv["NODE_ENV"];
+
+/** @private */
+const devEnvs: Partial<Record<Env, unknown>> = {
+	development: null,
+	test: null,
+} as const;
+
 /** @public */
 const client = new Sequelize(process.env.DATABASE_URL, {
 	dialect: "postgres",
 	dialectOptions: <ClientConfig> {
-		ssl: {
-			require: true,
+		ssl: process.env.NODE_ENV in devEnvs ? false : {
+			require: false,
 			rejectUnauthorized: false, // allow self-signed certificates
 		},
 	},
